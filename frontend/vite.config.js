8788d96ccdc9f06d09ae1965dde1e799fe7280c5
bhanupattemz <127473015+bhanupattemz@users.noreply.github.com>
2024-09-08 17:13:57 +0530

    update vite.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This handles routing in development when refreshing non-root routes
+    historyApiFallback: true,
+  },
 })
+
 
 
 // import { defineConfig } from 'vite';
